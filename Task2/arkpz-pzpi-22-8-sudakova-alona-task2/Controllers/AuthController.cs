c682daf02d939f695577ac60133a576b038dfaae
Alona Sudakova <alona.sudakova05@gmail.com>
2024-12-16 22:42:54 +0100

    Add files via upload

---
+++ Task2/arkpz-pzpi-22-8-sudakova-alona-task2/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Mvc;
+using FocusLearn.Models;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using FocusLearn.Repositories.Abstract;
+
+namespace FocusLearn.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        /// <summary>
+        /// Авторизація за допомогою Google
+        /// </summary>
+        [HttpGet("login-google")]
+        public IActionResult LoginGoogle()
+        {
+            var redirectUrl = Url.Action(nameof(GoogleResponse), "Auth");
+            return Challenge(new AuthenticationProperties { RedirectUri = redirectUrl }, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("google-response")]
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            if (result?.Principal == null)
+                return Unauthorized("Google authentication failed.");
+
+            var token = await _authService.AuthenticateGoogleUserAsync(result.Principal);
+            return Ok(new { message = "Logged in successfully.", token });
+        }
+
+        /// <summary>
+        /// Авторизація за допомогою Facebook
+        /// </summary>
+        [HttpGet("login-facebook")]
+        public IActionResult LoginFacebook()
+        {
+            var redirectUrl = Url.Action(nameof(FacebookResponse), "Auth");
+            return Challenge(new AuthenticationProperties { RedirectUri = redirectUrl }, FacebookDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("facebook-response")]
+        public async Task<IActionResult> FacebookResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            if (result?.Principal == null)
+                return Unauthorized("Authentication failed.");
+
+            var token = await _authService.AuthenticateFacebookUserAsync(result.Principal);
+            return Ok(new { message = "Logged in successfully.", token });
+        }
+    }
+}
---
+++ Task2/arkpz-pzpi-22-8-sudakova-alona-task2/Controllers/UsersController.cs
@@ -0,0 +1,138 @@
+﻿using FocusLearn.Models.DTO;
+using FocusLearn.Models.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FocusLearn.Repositories.Abstract;
+using FocusLearn.Repositories.Implementation;
+using System.Security.Claims;
+
+
+namespace FocusLearn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize] // Доступ лише для авторизованих користувачів
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _service;
+        public UsersController(IUserService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// Отримати користувача за ідентифікатором
+        /// </summary>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _service.GetUserByIdAsync(id);
+
+            if (user == null)
+                return NotFound("User with inserted ID not found.");
+
+            return Ok(user);
+        }
+
+        /// <summary>
+        /// Отримати всіх користувачів (доступно лише адміністраторам)
+        /// </summary>
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _service.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        /// <summary>
+        /// Отримати всіх користувачів з роллю репетитора
+        /// </summary>
+        [HttpGet("tutors")]
+        public async Task<IActionResult> GetAllTutors()
+        {
+            var users = await _service.GetAllTutorsAsync();
+            return Ok(users);
+        }
+
+        /// <summary>
+        /// Отримати інформацію з особистого профілю
+        /// </summary>
+        [HttpGet("my-profile")]
+        public async Task<IActionResult> GetMyProfile()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier) ??
+                      User.FindFirst("sub") ??
+                      User.FindFirst("UserId");
+
+            if (userIdClaim == null)
+            {
+                return Unauthorized("User ID not found in token");
+            }
+
+            var userId = int.Parse(userIdClaim.Value);
+
+            var user = await _service.GetUserByIdAsync(userId);
+
+            if (user == null)
+                return NotFound("User not found.");
+
+            return Ok(user);
+        }
+
+        /// <summary>
+        /// Редагувати інформацію особистого профілю
+        /// </summary>
+        [HttpPut("my-profile")]
+        public async Task<IActionResult> UpdateMyProfile([FromBody] UpdateProfileDTO updateUserDto)
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier) ??
+                                  User.FindFirst("sub") ??
+                                  User.FindFirst("UserId");
+
+
+            if (userIdClaim == null)
+            {
+                return Unauthorized("User ID not found in token");
+            }
+
+            var userId = int.Parse(userIdClaim.Value);
+
+            var result = await _service.UpdateUserAsync(userId, updateUserDto);
+
+            if (!result)
+                return NotFound("User not found.");
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Видалити особистий профіль
+        /// </summary>
+        [HttpDelete("my-profile")]
+        public async Task<IActionResult> DeleteMyProfile()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier) ??
+                      User.FindFirst("sub") ??
+                      User.FindFirst("UserId");
+
+            if (userIdClaim == null)
+            {
+                return Unauthorized("User ID not found in token");
+            }
+            var userId = int.Parse(userIdClaim.Value);
+
+
+            var result = await _service.DeleteUserAsync(userId);
+
+            if (!result)
+                return NotFound("User not found.");
+
+            return NoContent();
+        }
+
+    }
+}
+
