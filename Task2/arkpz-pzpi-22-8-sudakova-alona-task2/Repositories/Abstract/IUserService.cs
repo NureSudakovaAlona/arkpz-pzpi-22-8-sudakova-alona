809563fcdaaa2ba458ac76b48f616db5919b6036
Alona Sudakova <alona.sudakova05@gmail.com>
2024-12-16 22:47:24 +0100

    Add files via upload

---
+++ Task2/arkpz-pzpi-22-8-sudakova-alona-task2/Repositories/Abstract/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using FocusLearn.Models.Domain;
+using FocusLearn.Models.DTO;
+
+namespace FocusLearn.Repositories.Abstract
+{
+    public interface IUserService
+    {
+        Task AddUserAsync(User user);
+        Task<User?> GetUserByProviderAsync(string providerId, string authProvider);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<UserDTO?> GetUserByIdAsync(int userId);
+        Task<bool> UpdateUserAsync(int userId, UpdateProfileDTO updateUserDto);
+        Task<bool> DeleteUserAsync(int userId);
+        Task<IEnumerable<UserDTO>> GetAllUsersAsync();
+        Task<IEnumerable<UserDTO>> GetAllTutorsAsync();
+    }
+}
