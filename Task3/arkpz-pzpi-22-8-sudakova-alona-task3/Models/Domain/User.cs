3034d70e52debe860a41889f16c98a7f0b5541f0
Alona Sudakova <alona.sudakova05@gmail.com>
2024-12-20 22:42:52 +0100

    Models updated

---
+++ Task3/arkpz-pzpi-22-8-sudakova-alona-task3/Models/Domain/User.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace FocusLearn.Models.Domain;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? Role { get; set; }
+
+    public string? ProfilePhoto { get; set; }
+
+    public string? Language { get; set; }
+
+    public string? ProfileStatus { get; set; }
+
+    public string? AuthProvider { get; set; }
+
+    public string? ProviderId { get; set; }
+
+    public DateTime? RegistrationDate { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<Assignment> AssignmentStudents { get; } = new List<Assignment>();
+    [JsonIgnore]
+    public virtual ICollection<Assignment> AssignmentTutors { get; } = new List<Assignment>();
+    [JsonIgnore]
+    public virtual ICollection<IoTSession> IoTSessions { get; } = new List<IoTSession>();
+    [JsonIgnore]
+    public virtual ICollection<LearningMaterial> LearningMaterials { get; } = new List<LearningMaterial>();
+    [JsonIgnore]
+    public virtual ICollection<UserMethodStatistics> UserMethodStatistics { get; } = new List<UserMethodStatistics>();
+}
