5df26f91ff02b51adc3ce0134c8fa9fdadb6dc44
Alona Sudakova <alona.sudakova05@gmail.com>
2024-12-20 22:44:37 +0100

    DTO updated

---
+++ Task3/arkpz-pzpi-22-8-sudakova-alona-task3/Models/DTO/UpdateProfileDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FocusLearn.Models.DTO
+{
+    public class UpdateProfileDTO
+    {
+        public string? UserName { get; set; }
+        public string? ProfilePhoto { get; set; }
+        public string? Language { get; set; }
+    }
+}
