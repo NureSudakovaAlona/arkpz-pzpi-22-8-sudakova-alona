5a8b2290e9ca71ba8afa477ddaa01c79fa0e53c8
Alona Sudakova <alona.sudakova05@gmail.com>
2024-12-20 22:47:57 +0100

    Inrerfaces updated

---
+++ Task3/arkpz-pzpi-22-8-sudakova-alona-task3/Repositories/Abstract/ILearningMaterialService.cs
@@ -0,0 +1,14 @@
+﻿using FocusLearn.Models.Domain;
+using FocusLearn.Models.DTO;
+
+namespace FocusLearn.Repositories.Abstract
+{
+    public interface ILearningMaterialService
+    {
+        Task<IEnumerable<LearningMaterialDTO>> GetAllMaterialsAsync();
+        Task<LearningMaterialDTO?> GetMaterialByIdAsync(int id);
+        Task AddMaterialAsync(int creatorId, LearningMaterialDTO materialDto);
+        Task<bool> UpdateMaterialAsync(int id, LearningMaterialDTO materialDto);
+        Task<bool> DeleteMaterialAsync(int id);
+    }
+}
