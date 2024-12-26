24890b4fe3c86b908b5e07b75660dc2f1b723900
Alona Sudakova <alona.sudakova05@gmail.com>
2024-12-26 21:58:18 +0100

    Add files via upload

---
+++ Task5/arkpz-pzpi-22-8-sudakova-alona-task5/Models/Domain/ConcentrationMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FocusLearn.Models.Domain;
+
+public partial class ConcentrationMethod
+{
+    public int MethodId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int WorkDuration { get; set; }
+
+    public int BreakDuration { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<IoTSession> IoTSessions { get; } = new List<IoTSession>();
+
+    public virtual ICollection<UserMethodStatistics> UserMethodStatistics { get; } = new List<UserMethodStatistics>();
+}
