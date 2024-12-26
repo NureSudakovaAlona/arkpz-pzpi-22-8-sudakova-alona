56102f46d7c25adfb8677694d5bef7242b810f78
Alona Sudakova <alona.sudakova05@gmail.com>
2024-12-26 22:03:20 +0100

    Add files via upload

---
+++ Task5/arkpz-pzpi-22-8-sudakova-alona-task5/Repositories/Abstract/IIoTSessionService.cs
@@ -0,0 +1,10 @@
+﻿using FocusLearn.Models.DTO;
+namespace FocusLearn.Repositories.Abstract
+{
+    public interface IIoTSessionService
+    {
+        Task<bool> AddIoTSessionAsync(IoTSessionDTO sessionDto);
+        Task<IEnumerable<IoTSessionDTO>> GetIoTSessionsByUserIdAsync(int userId);
+        Task<bool> SaveIoTSessionAsync(IoTSessionDTO sessionDto);
+    }
+}
